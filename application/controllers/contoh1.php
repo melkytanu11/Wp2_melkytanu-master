d1c3b5a7d5656b1f7aa8c606708bb969a6e0e38a
melkytanu093@gmail.com <you@example.com>
2022-09-26 19:23:13 +0700

    aupload

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Melky Tanu
+ Saya Kuliah di UBSI Pontianak
+ Hobby saya bermain Game";
+ }
+}
+?>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<h1>Nama saya Melky Tanu</h1>
+<h2>Kuliah di UBSI Pontianak</h2>
+<h3>Hobby saya bermain Game</h3>
+</html>
